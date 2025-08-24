cf8e91f387a6b421d121a1e29f691c8a76bc9442
Root0073 <2720880732@qq.com>
2025-08-24 21:41:34 +0800

    新增矩阵快速幂

--- 牛客/2025河南萌新赛/第五场/K.cpp
+++ 牛客/2025河南萌新赛/第五场/K.cpp
@@ -5,12 +5,19 @@ int main() {
     int sx, sy, tx, ty;
     cin >> sx >> sy >> tx >> ty;
     int ans = inf;
+    bool flag = false;
     auto dfs = [&](auto &dfs, int x, int y, int s) {
         if(x > tx || y > ty) return ;
-        if(x == tx && u == ty) {
+        if(x == tx && y == ty) {
             ans = min(ans, s);
+            flag = true;
             return ;
         }
-        
-    }
+        int add = max(x, y);
+        dfs(dfs, x + add, y, s + 1);
+        dfs(dfs, x, y + add, s + 1);
+    };
+    dfs(dfs, sx, sy, 0);
+    if(flag) cout << ans << endl;
+    else cout << -1 << endl;
 }
