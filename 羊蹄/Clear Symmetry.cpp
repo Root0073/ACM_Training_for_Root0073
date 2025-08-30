7b7df924adff4b2c39e8504d73369e1d40ea36f1
Root0073 <2720880732@qq.com>
2025-08-31 00:34:19 +0800

    cf201A

---
+++ 羊蹄/Clear Symmetry.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int x;
+	cin >> x;
+	//1 0 1
+	//0 1 0 
+	//1 0 1
+	//5
+//可以发现如果用偶数构造矩阵他的利用率非常低
+	//0 1 0 1 0
+	//1 0 1 0 1
+	//0 1 0 1 0
+	//1 0 1 0 1
+	//0 1 0 1 0
+	//8 - 13
+	vector<int> a = {1, 5, 13, 25, 41, 61, 85, 113};
+	for(int i = 0; i < 8; i++) {
+		if(x > a[i]) continue;
+		else {
+			cout << i * 2 + 1 << endl;
+			return 0;
+		}
+	}
+}
